288544b05c2a3fbd2d3e0ed416eca219b61e3a02
Bulat <kusaeff.b@gmail.com>
2026-06-05 22:46:01 +0400

    Initial flow-manager service

---
+++ src/main/java/ru/adnr/flowmanager/FlowManagerApplication.java
@@ -0,0 +1,12 @@
+package ru.adnr.flowmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowManagerApplication.class, args);
+    }
+}
---
+++ src/main/java/ru/adnr/flowmanager/controller/package-info.java
@@ -0,0 +1,1 @@
+package ru.adnr.flowmanager.controller;
